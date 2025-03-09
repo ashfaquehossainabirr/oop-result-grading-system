5a5fc6a3085c9f789cf9320def27e7974ee0cc49
Ashfaque Hossain Abir <ashfaquehossainabirr@gmail.com>
2025-03-09 19:50:13 +0600

    Updated

---
+++ js/studentList.js
@@ -0,0 +1,40 @@
+const students = {
+    student1: {
+        id: 1023,
+        name: "Ashfaque Hossain Abir",
+        email: "ashfaquehossain@github.com",
+        marks: 87
+    },
+    student2: {
+        id: 1024,
+        name: "Mohammed Sadeq",
+        email: "mohammedsadeq@mail.com",
+        marks: 67
+    },
+    student3: {
+        id: 1025,
+        name: "Ahsanullah Babu",
+        email: "asanullababu@google.com",
+        marks: 73
+    },
+    student4: {
+        id: 1026,
+        name: "Raihanul Al Nahian",
+        email: "alnahian2003@github.com",
+        marks: 43
+    },
+    student5: {
+        id: 1027,
+        name: "Yasin Chowdhury",
+        email: "yasin2000@mail.com",
+        marks: 37
+    },
+    student6: {
+        id: 1029,
+        name: "Robiul Islam Chamak",
+        email: "chamak302@google.com",
+        marks: 78
+    },
+}
+
+console.log(students)
